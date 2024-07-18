ef0a5e184d1a640f758d15b02a6d62d369ad61e8
DAVI SILVA <davi.silva403@etec.sp.gov.br>
2024-07-18 20:51:09 -0300

    Main page route and colors settings updated

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -7,7 +7,7 @@ namespace MauiAppHotel
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.HiringAccommodation());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
