cb3d5bec2f9dde2bf0d1115cb8d14d52d1d85954
DAVI SILVA <davi.silva403@etec.sp.gov.br>
2024-07-19 17:55:30 -0300

    Reapply "Room class creation/configuration, creation of rooms and their properties, and giving functionality to the room picker."
    This reverts commit 68c6cf5e75c5c3a108e4c68cf76e3c132ca179fb.

---
+++ MauiAppHotel/Models/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Models
+{
+    public class Room
+    {
+        public string? Description { get; set; }
+        public double AdultDiaryValue { get; set; }
+        public double ChildrenDiaryValue { get; set; }
+
+    }
+}
--- MauiAppHotel/Views/HiredAccommodation.xaml.cs
+++ MauiAppHotel/Views/HiredAccommodation.xaml.cs
@@ -6,4 +6,17 @@ public partial class HiredAccommodation : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Navigation.PopAsync();
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("OPS!", ex.Message, "OK");
+		}
+	}
+
 }
