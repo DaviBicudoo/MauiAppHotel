7a1a53242c708e0c6c9fdf347f9f5e7aea8c322d
DAVI SILVA <davi.silva403@etec.sp.gov.br>
2024-07-19 17:36:19 -0300

    Room class creation/configuartion, creation of rooms and their properties and giving functionality to the room picker.

--- MauiAppHotel/Views/HiringAccommodation.xaml.cs
+++ MauiAppHotel/Views/HiringAccommodation.xaml.cs
@@ -2,8 +2,25 @@ namespace MauiAppHotel.Views;
 
 public partial class HiringAccommodation : ContentPage
 {
+	App appProperties;
 	public HiringAccommodation()
 	{
 		InitializeComponent();
+
+		appProperties = (App)Application.Current;
+
+		roomPicker.ItemsSource = appProperties.RoomList;
 	}
+
+    private void NextButton_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new HiredAccommodation());
+
+		} catch(Exception ex)
+		{
+			DisplayAlert("Ops!", ex.Message, "OK");
+		}
+    }
 }
