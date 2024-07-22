c94c005561e3d5e9387547821bb36be0fae6dc97
DAVI SILVA <davi.silva403@etec.sp.gov.br>
2024-07-22 14:25:10 -0300

    Accommodation model class created

---
+++ MauiAppHotel/Models/Accommodation.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiAppHotel.Models
+{
+    public class Accommodation
+    {
+        public Room SelectedRoom { get; set; }
+
+        public int NumberOfAdults { get; set; }
+        public int NumberOfChildren { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+
+        public int TimeOfStay
+        {
+            get => CheckOutDate.Subtract(CheckInDate).Days;
+        }
+
+        public double TotalValue
+        {
+            get
+            {
+                double adultsValue = NumberOfAdults * SelectedRoom.AdultDiaryValue;
+                double childrenValue = NumberOfChildren * SelectedRoom.ChildrenDiaryValue;
+
+                double total = (adultsValue + childrenValue) * TimeOfStay;
+
+                return total;
+            }
+        }
+    }
+}
